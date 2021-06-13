78f87eacda4be13a0b0150094d97fd1cd955e09d
Mark Wallace <mark.wallace6@gmail.com>
2021-06-13 17:54:55 -0500

    wired up passport done in db requests

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express'); 
-const mongoose = require('mongoose');
-const keys = require('./config/keys');
-require('./models/User'); //load model before passport!
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/User"); //load model before passport!
+require("./services/passport");
 
 // new instance of express server named app
-const app = express(); 
+const app = express();
 console.log("Server Started");
 
 //authRoutes just needs to be started and it requires  the express server
 //so we require it, get a function back, then immediately pass in the app object
-require('./routes/authRoutes')(app);
+require("./routes/authRoutes")(app);
 
 mongoose.connect(keys.mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 //dynamic port configuration. Use Heroku's env var or use 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT); 
+app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -1,15 +1,30 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
-const User = mongoose.model('users');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+const User = mongoose.model("users");
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    //callback function after the user successfully auths
-}, (accessToken, refreshToken, profile, done) => {
-    //this next bit creates a new user instance and saves it to the collection
-    new User({ googleId: profile.id }).save();
-}));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      //callback function after the user successfully auths
+    },
+    (accessToken, refreshToken, profile, done) => {
+      //next we check if this user already exists in the DB
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          //we already have this guy, so lets call passports done function
+          done(null, existingUser);
+        } else {
+          //this next bit creates a new user instance and saves it to the collection
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
+    }
+  )
+);
