59abc7396562eaeed41a45a9d92cb739acd40eda
Mark Wallace <mark.wallace6@gmail.com>
2021-06-12 21:20:22 -0500

    pre refactor

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ const keys = require('./config/keys');
 
 // new instance of express server named app
 const app = express(); 
-
+console.log("Server Started");
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, accessToken => {
-    console.log(accessToken);
+}, (accessToken, refreshToken, profile, done) => {
+    console.log('access token :', accessToken);
+    console.log('refresh token :', refreshToken);
+    console.log('profile:', profile);
 }));
 
 //route handler
@@ -22,6 +24,8 @@ app.get('/auth/google',
 passport.authenticate('google', {
     scope: ['profile', 'email']
 }))
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 //dynamic port configuration. Use Heroku's env var or use 5000
 const PORT = process.env.PORT || 5000;
