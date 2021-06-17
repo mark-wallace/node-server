bad7d3f48989cbe466c3ac8996b912975628d2a5
Mark Wallace <mark.wallace6@gmail.com>
2021-06-16 21:13:51 -0500

    react router boilerplate

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveysNew = () => <h2>SurveysNew</h2>;
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/surveys" component={Dashboard} />
+        <Route path="/surveys/new" component={SurveysNew} />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
