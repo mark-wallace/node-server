02fe3cd78762c7d86ca332aca1d91e3e92c51086
Mark Wallace <mark.wallace6@gmail.com>
2021-06-16 21:49:58 -0500

    created separate header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
-const Header = () => <h2>Header</h2>;
+import Header from "./Header";
 const Landing = () => <h2>Landing</h2>;
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveysNew = () => <h2>SurveysNew</h2>;
