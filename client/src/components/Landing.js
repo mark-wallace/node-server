520c3e12acb43a9d8ebd7fd6d1fe80ab0d9547ec
Mark Wallace <mark.wallace6@gmail.com>
2021-06-19 15:18:47 -0500

    created landing component, finished header

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Emaily!</h1>
+      Survey. Learn. Improve. <br />
+      Easy.
+    </div>
+  );
+};
+
+export default Landing;
