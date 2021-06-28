da633e35f1da6046fc49f698df4570d7e99f5211
Mark Wallace <mark.wallace6@gmail.com>
2021-06-28 16:45:52 -0500

    email working

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,15 @@
 module.exports = (survey) => {
-  return "<div>" + survey.body + "</div>";
+  return `
+    <html>
+        <body>
+            <div style="text-align: center;">
+                <h3>We'd Like Your Feedback!</h3>
+                <p>Please answer the following question:</p>
+                <p>${survey.body}</p>
+                <a href="http://localhost:3000">Yes</a>
+                <a href="http://localhost:3000">No</a>
+            </div>
+        </body>
+    </html>
+  `;
 };
