1f7b444ad8598d2230d6082bcb8fb9d4c5ef1380
Mark Wallace <mark.wallace6@gmail.com>
2021-06-30 15:43:13 -0500

    started adding surveys using redux-form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from "../actions";
 
 import Header from "./Header";
 import Landing from "./Landing";
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveysNew = () => <h2>SurveysNew</h2>;
+import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 
 class App extends Component {
   componentDidMount() {
@@ -20,7 +20,7 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Landing} />
           <Route exact path="/surveys" component={Dashboard} />
-          <Route path="/surveys/new" component={SurveysNew} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </BrowserRouter>
       </div>
     );
